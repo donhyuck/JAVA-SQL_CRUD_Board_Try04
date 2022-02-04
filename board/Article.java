b16fd2c531dec92c8a16d6ebd28d0584ee273fb9
donhyuck <leedonhyuck123@gmail.com>
2022-02-04 22:58:14 +0900

    게시글 작성 기능 추가

---
+++ board/Article.java
@@ -0,0 +1,16 @@
+package board;
+
+public class Article {
+
+	public int id;
+	public String title;
+	public String body;
+
+	public Article(int id, String title, String body) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
+}
--- board/boardMain.java
+++ board/boardMain.java
@@ -1,15 +1,22 @@
 package board;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class boardMain {
+public class BoardMain {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
+		List<Article> articles = new ArrayList<>();
 
 		System.out.println("== 프로그램 시작 ==");
 
+		int lastArticleId = 1;
+
 		while (true) {
+
 			System.out.print("명령어 : ");
 			String cmd = sc.nextLine();
 
@@ -22,17 +29,26 @@ public class boardMain {
 			if (cmd.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
-				
+
 			} else if (cmd.equals("article write")) {
 
 				System.out.println("== 게시글 작성 ==");
-				// 구현중
+
+				int id = lastArticleId;
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				System.out.printf("%d번 게시글이 등록되었습니다.\n", id);
+				lastArticleId++;
+
 			} else {
 				System.out.printf("%s는 잘못된 명령어입니다.\n", cmd);
 			}
-
 		}
-
 	}
-
 }
