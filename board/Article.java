1162d937ebf98f5f95a8eda367ba7ceb9b2be9c1
donhyuck <leedonhyuck123@gmail.com>
2022-02-04 23:40:15 +0900

    JDBC연결 후 게시글 작성 기능 수정

--- board/Article.java
+++ board/Article.java
@@ -3,12 +3,16 @@ package board;
 public class Article {
 
 	public int id;
+	public String regDate;
+	public String updateDate;
 	public String title;
 	public String body;
 
-	public Article(int id, String title, String body) {
+	public Article(int id, String regDate, String updateDate, String title, String body) {
 		super();
 		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
 		this.title = title;
 		this.body = body;
 	}
--- board/boardMain.java
+++ board/boardMain.java
@@ -1,7 +1,9 @@
 package board;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class BoardMain {
@@ -9,11 +11,8 @@ public class BoardMain {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		List<Article> articles = new ArrayList<>();
 
 		System.out.println("== 프로그램 시작 ==");
-
-		int lastArticleId = 1;
 
 		while (true) {
 
@@ -34,17 +33,53 @@ public class BoardMain {
 
 				System.out.println("== 게시글 작성 ==");
 
-				int id = lastArticleId;
 				System.out.print("제목 : ");
 				String title = sc.nextLine();
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
 
-				Article article = new Article(id, title, body);
-				articles.add(article);
+				// JDBC적용
+				Connection conn = null; // DB 접속 객체
+				PreparedStatement pstat = null; // SQL 구문을 실행하는 역할
 
-				System.out.printf("%d번 게시글이 등록되었습니다.\n", id);
-				lastArticleId++;
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver"); // Mysql JDBC 드라이버 로딩
+					String url = "jdbc:mysql://127.0.0.1:3306/DB_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title = \"" + title + "\"";
+					sql += ", body = \"" + body + "\"";
+
+					pstat = conn.prepareStatement(sql);
+					int affectedRows = pstat.executeUpdate();
+
+					System.out.printf("게시글이 등록되었습니다.\n");
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally { // 예외 상황이든 아니든 무조건 마지막에 실행하는 finally
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close(); // 연결 종료
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+					try {
+						if (pstat != null && !pstat.isClosed()) {
+							pstat.close(); // 연결 종료
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 
 			} else {
 				System.out.printf("%s는 잘못된 명령어입니다.\n", cmd);
